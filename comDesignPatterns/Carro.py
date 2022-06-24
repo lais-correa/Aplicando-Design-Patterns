f822390a95f8bccb66f54440463889ca22abe9ae
Patricia de Moraes <patriciawentzm@gmail.com>
2022-06-24 18:02:38 -0300

    :sparkles: adicionando classe Carro.py

---
+++ comDesignPatterns/Carro.py
@@ -0,0 +1,26 @@
+from Veiculo import Veiculo
+
+class Carro(Veiculo):
+    def __init__(self,marca, modelo, ano):
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__ano = ano
+        self.velocidade = 0
+
+    def acelerar(self, valor):
+        self.velocidade = self.velocidade + valor
+        return self.velocidade
+
+    def frear(self, valor):
+        if self.velocidade > 0:
+            self.velocidade = self.velocidade - valor
+        else:
+            print(f'\nO veículo já está parado, portanto não há como frear.')
+        return self.velocidade
+    
+    def printVeiculo(self):
+        print(f"""
+            A marca do que carro é: {self.__marca}
+            O modelo é: {self.__modelo}
+            O ano é: {self.__ano}
+            A velocidade é: {self.velocidade} km/h""")
