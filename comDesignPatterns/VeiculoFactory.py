e22dbc6d9094f1ba3b676e748f05d098aa7f5892
Lais Correa <laiscorreamartins@gmail.com>
2022-06-25 10:41:39 -0300

    :sparkles: adicionando novas features em VeiculoFactory.py

--- comDesignPatterns/VeiculoFactory.py
+++ comDesignPatterns/VeiculoFactory.py
@@ -1,4 +1,6 @@
 from Carro import Carro
+from Moto import Moto
+from Bicicleta import Bicicleta
 
 class VeiculoFactory:
     def criar_veiculo(self,nome):
@@ -8,4 +10,15 @@ class VeiculoFactory:
             ano = input('Qual o ano do seu carro? ')
             return Carro(marca, modelo, ano)
 
+        elif nome == 'MOTO':
+            marca = input('Qual a marca da sua moto? ')
+            modelo = input('Qual o modelo da sua moto? ')
+            ano = input('Qual o ano da sua moto? ')
+            return Moto(marca, modelo, ano)
+
+        elif nome == 'BICICLETA':
+            marca = input('Qual a marca da sua bicicleta? ')
+            modelo = input('Qual o modelo da sua bicicleta? ')
+            ano = input('Qual o ano da sua bicicleta? ')
+            return Bicicleta(marca, modelo, ano)
     
